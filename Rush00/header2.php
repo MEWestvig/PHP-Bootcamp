bf0b70a67a5821a88e513a30600c6340f0ef2327
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-10-07 20:25:41 +0200

    Finishing touches

--- Rush00/header2.php
+++ Rush00/header2.php
@@ -17,6 +17,7 @@ echo '
       <li><a class="left" href="contact.php">Contact</a></li>';
        if ($_SESSION['curr_user']){
          echo '<li><a class="right" href="logout.php">Logout</a></li>';
+         echo '<li><a class="right" href="modify.php">Change pass</a></li>';
         }else {
           echo '<li><a class="right" href="create.php">Register</a></li>
                 <li><a class="right" href="login.php">Login</a></li>';

e58dc4d7581bdb75a427a4040683176d5d36de00
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-10-11 22:50:36 +0200

    Updating d08

--- d08/ex00/Player.class.php
+++ d08/ex00/Player.class.php
@@ -5,16 +5,17 @@ class Player
 	protected $name = "";
 	protected $x = 0;
 	protected $y = 0;
-	function __construct($n, $num)
+	protected $movement = 15;
+	function __construct($n, $num, $board_x, $board_y)
 	{
 		$this->name = $n;
-		if ($num)
+		if (!$num)
 		{
-			$this->x = rand(0, 75);
-			$this->y = rand(0, 50);
+			$this->x = rand(0, $board_x / 8);
+			$this->y = rand(0, $board_y / 8);
 		} else {
-			$this->x = rand(75, 150);
-			$this->y = rand(50, 100);
+			$this->x = rand($board_x / 2 + $board_x / 2 / 4 * 3, $board_x);
+			$this->y = rand($board_y / 2 + $board_y / 2 / 4 * 3, $board_y);
 		}
 	}
 	function play()
@@ -32,6 +33,10 @@ class Player
 	function getY()
 	{
 		return $this->y;
+	}
+	function getMove()
+	{
+		return $this->movement;
 	}
 }
 ?>
--- d08/ex00/index.php
+++ d08/ex00/index.php
@@ -6,9 +6,12 @@ if (empty($_POST))
   header('Location: '. "new_game.php");
 
 $name = "Daniel";
-$p1 = new Player($_POST['p1'], 0);
-$p2 = new Player($_POST['p2'], 1);
+$board_y = 10;
+$board_x = 10;
+$p1 = new Player($_POST['p1'], 0, $board_x, $board_y);
+$p2 = new Player($_POST['p2'], 1, $board_x, $board_y);
 $d6 = new Dice();
+
 ?>
 <html>
 <head>
@@ -28,24 +31,37 @@ $d6 = new Dice();
        <th>Player 2</th>
      </tr>
      <tr>
-       <td><?php print($p1->getName()); ?></td>
-       <td><?php print($p2->getName()); ?></td>
+       <td style="text-align: center"><?php print($p1->getName()); ?></td>
+       <td style="text-align: center"><?php print($p2->getName()); ?></td>
      </tr>
    </table>
    <table class = "grid">
      <?php
-     for ($i=0; $i < 100; $i++) {
+     for ($i=0; $i < $board_y; $i++) {
        echo '<tr class = "gridtr">';
-       for ($j=0; $j < 150; $j++) {
-        if ($p1->getY === $i && $p1->getX === $j)
-          echo '<td bgcolor="#000000" class = "gridtd">Hello</td>';
+       for ($j=0; $j < $board_x; $j++) {
+        if ($p1->getY() === $i && $p1->getX() === $j)
+          echo '<td bgcolor="GREEN" class = "gridtd"></td>';
+        else if ($p2->getY() === $i && $p2->getX() === $j)
+          echo '<td bgcolor="RED" class = "gridtd"></td>';
         else
-          echo '<td class = "gridtd"></td>';
+        {
+          if ((($j <= $p1->getX() + $p1->getMove() && $j >= $p1->getX() - $p1->getMove()) && $i === $p1->getY()) ||
+          (($i <= $p1->getY() + $p1->getMove() && $i >= $p1->getY() - $p1->getMove()) && $j === $p1->getX()))
+          {
+            print($i ." ". $j . " \n");
+            echo '<td bgcolor="orange" class = "gridtd"><a href="#child4" class="parent" style="display: block;width = calc(100% / 150);height: calc(100vw / 150);"></a></td>';
+          }
+          else
+            echo '<td bgcolor="white" class = "gridtd"></td>';
+        }
        }
        echo '</tr>';
      }
-     print ($p1->getX() . PHP_EOL);
-     print ($p1->getY() . PHP_EOL);
+     print ("Player x: " . $p1->getX() . "\n");
+     print ("Player Y: " . $p1->getY() . "\n");
+     print ("Player x: " . $p2->getX() . "\n");
+     print ("Player Y: " . $p2->getY() . "\n");
      ?>
    </table>
  </body>
